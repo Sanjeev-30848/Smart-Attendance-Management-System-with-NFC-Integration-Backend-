0c0a6dcabbe471c0c669ece89389f625784029b4
venkata sanjeev <jagansanjeev2007@gmail.com>
2026-06-22 17:00:27 +0530

    Created Basic Project Stucture

---
+++ src/main/java/com/klef/attendance/SmartAttendanceSystemApplication.java
@@ -0,0 +1,14 @@
+package com.klef.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceSystemApplication.class, args);
+		System.out.println("Smart Attendance Management System is running...");
+	}
+
+}
---
+++ src/test/java/com/klef/attendance/SmartAttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
